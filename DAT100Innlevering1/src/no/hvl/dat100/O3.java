e6f900ca47522646122ea7ad568e7956ac34a382
LuciferDeFallen <53998864+LuciferDeFallen@users.noreply.github.com>
2024-09-18 14:15:05 +0200

    update increment

--- DAT100Innlevering1/src/no/hvl/dat100/O3.java
+++ DAT100Innlevering1/src/no/hvl/dat100/O3.java
@@ -6,15 +6,16 @@ public class O3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(factorial(1000));
+		System.out.println(factorial(4));
+		System.out.println(factorial(5));
+		System.out.println(factorial(5000));
 	}
 	
 	public static BigInteger factorial(int n) {
 		if (n <= 0) return BigInteger.valueOf(0);
 		BigInteger o = BigInteger.valueOf(n);
 		while (n > 1) {
-			n--;
-			o = o.multiply(BigInteger.valueOf(n));
+			o = o.multiply(BigInteger.valueOf(--n));
 		}
 		return o;
 	}
